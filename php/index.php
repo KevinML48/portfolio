cb10fdb9b4a44a11c4fcb228b1a23cff74645405
Kevin <kevinpromogreta@gmail.com>
2022-12-07 14:10:33 +0100

    update de fou
    commit

---
+++ php/index.php
@@ -0,0 +1,18 @@
+
+<?php
+
+    <div class="container">
+        
+        <div class="strater-template">
+            <form action="post_contact.php" method="POST">
+                 <div class="row">
+                    <div class="col-xs-6">
+                </div>
+                <div class="forms-group">
+                    <label for="inputname">Votre nom</label>
+                    <imput type="text" name="name" class="form-control" id="inputname">
+                </div>
+            </form>
+        </div>
+
+?>
---
+++ php/send.php
@@ -0,0 +1,8 @@
+<?php
+$data=array(
+    "mail"=> kevinpromogreta@gmail.com
+    "objet"=> NOM,dATE: openssl_csr_get_subject
+    "message"=> contenue message
+    "isHTM"=>'true'
+);
+?>
